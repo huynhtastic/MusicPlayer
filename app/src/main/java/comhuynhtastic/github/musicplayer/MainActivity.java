6de788e25357fc60a48765b36feab0fa59ce0788
Richard Huynh <huynhtastic@gmail.com>
2016-08-08 17:57:28 -0500

    initial commit; can play, pause, and seek

---
+++ app/src/main/java/comhuynhtastic/github/musicplayer/MainActivity.java
@@ -0,0 +1,51 @@
+package comhuynhtastic.github.musicplayer;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
+    private Button btnPlay;
+    private Button btnPause;
+    private Button btnSeek;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.miraisekai);
+
+        btnPlay = (Button) findViewById(R.id.button_play);
+        btnPause = (Button) findViewById(R.id.button_pause);
+        btnSeek = (Button) findViewById(R.id.button_seek_five);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
+            }
+        });
+
+        btnSeek.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() + 5000);
+            }
+        });
+    }
+
+
+}
